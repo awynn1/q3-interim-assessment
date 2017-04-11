aa2fe80a83c55b5ecd718b2d6ccad7da9c991e53
Antalia Wynn <awynn@wlapcs.org>
2017-04-11 17:09:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup(){
+    createCanvas(600, 400);
+    background(0);
+}
+
+function draw(){
+ strokeWeight(4);
+  fill(255,0,255);
+ 
+ ellipse(0, 255, 25, 25);
+ ellipse(50, 255, 25, 25);
+ ellipse(100, 255, 25, 25);
+ ellipse(150, 255, 25, 25);
+ ellipse(200, 255, 25, 25);
+ ellipse(250,255, 25, 25);
+ ellipse(300, 255, 25, 25);
+ ellipse(350, 255, 25, 25);
+ellipse(400, 255, 25, 25);
+ellipse(450, 255, 25, 25);
+ellipse(500, 255, 25, 25);
+ellipse(550, 255, 25, 25);
+ellipse(600, 255, 25, 25);
+}
